fe0801d5464fecfca632169d200515a1b7b04192
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-06-04 02:30:57 -0300

    criando esquema de rotas de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
+
     return (
-        <View>
-            <Text>GoBarber</Text>
-        </View>
+        <NavigationContainer>
+            <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+            <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+                <Routes />
+            </View>
+        </NavigationContainer>
     );
 }
 
