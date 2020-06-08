d3a10da3aa8ac8dea5cc1ad95a08e8b834813dcc
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-06-08 00:34:34 -0300

    criando componentes input e button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Alert } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Logo from '../../assets/logo.png';
+
 import { Container, Title } from './styles';
 
-import Logo from '../../assets/logo.png';
+
 
 const SignIn: React.FC = () => {
   return (
-      <Container>
-          <Image source={Logo}/>
-          <Title>Faça seu logon</Title>
-      </Container>
+    <Container>
+      <Image source={Logo} />
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-Mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => { }}>Entrar</Button>
+    </Container>
   )
 }
 
