5ca8a6bf5d69507239620c238760152335840a5a
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-06-08 22:27:42 -0300

    aplicando botos na login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import { View, Image, Alert } from 'react-native';
+import { View, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Logo from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles';
 
 
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={Logo} />
-      <Title>Faça seu logon</Title>
-      <Input name="email" icon="mail" placeholder="E-Mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button onPress={() => { }}>Entrar</Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+        keyboardShouldPersistTaps="handled"
+        style={{flex: 1}}
+        alwaysBounceVertical
+        >
+          <Container>
+            <Image source={Logo} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-Mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => { }}>Entrar</Button>
+            <ForgotPassword onPress={() => { }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
